e8608f5d3a5e70e7d59bae51cf4a1f16a2d74163
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-09 18:10:31 +0300

    Add Routing

--- event-management/routes/api.php
+++ event-management/routes/api.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('events', EventController::class);
+Route::apiResource('events.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'event']);
+
+// GET|HEAD api/event  =>  event.index
+// POST api/event  =>  event.store
+// GET|HEAD api/event/{event}  =>  event.show
+// PUT|PATCH api/event/{event}  =>  event.update
+// DELETE api/event/{event}  =>  event.delete
+// GET|HEAD api/event/{event}/attendees  =>  event.attendee.index
+// POST api/event/{event}/attendees  =>  event.attendee.store
+// GET|HEAD api/event/{event}/attendees/{attendee}  =>  event.attendee.show
+// PUT|PATCH api/event/{event}/attendees/{attendee}  =>  event.attendee.update
+// DELETE api/event/{event}/attendees/{attendee}  =>  event.attendee.delete
