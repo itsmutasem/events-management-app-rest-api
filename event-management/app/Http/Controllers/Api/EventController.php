e5d8f88cea75a5483a51801dc612aecc4e844c5a
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-11 20:41:32 +0300

    Return Events Data in the Index Page to Test API

--- event-management/app/Http/Controllers/Api/EventController.php
+++ event-management/app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class EventController extends Controller
 {
@@ -12,7 +13,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Models\Event::all();
     }
 
     /**
