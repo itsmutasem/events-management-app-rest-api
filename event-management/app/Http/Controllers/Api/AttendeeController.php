1c59b987524e30ef52c5f286e17906a214dd1566
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-22 14:15:16 +0300

    Store a New Attendee to an Event
    Creates a new attendee record in the attendees table with user_id = 1 (TEMP VALUE) and event_id automatically set (based on the event)

--- event-management/app/Http/Controllers/Api/AttendeeController.php
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -9,42 +9,29 @@ use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index(Event $event)
     {
         $attendees = $event->attendees()->latest();
         return AttendeeResource::collection($attendees->paginate());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            'user_id' => 1
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
