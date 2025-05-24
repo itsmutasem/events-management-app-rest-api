6ae3e7ef2be4696d2711d7c6a5938288a3f7c06e
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-24 16:34:31 +0300

    Fix Login Errors
    use Illuminate\Validation\ValidationException;
    
    and write the wrong function: ValidationException::withMessage

--- event-management/app/Http/Controllers/Api/AuthController.php
+++ event-management/app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Nette\Schema\ValidationException;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -17,12 +17,12 @@ class AuthController extends Controller
         ]);
         $user = \App\Models\User::where('email', $request->email)->first();
         if (!$user) {
-            throw ValidationException::withMessage([
+            throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect']
             ]);
         }
         if (!Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessage([
+            throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect']
             ]);
         }
