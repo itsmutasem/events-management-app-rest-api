ae89ff0960e9cbbf4a475e23b99ad3df152cd4c0
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-05-24 15:57:02 +0300

    Create Login & Logout Methods - Empty

--- event-management/app/Http/Controllers/Api/AuthController.php
+++ event-management/app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
 }
